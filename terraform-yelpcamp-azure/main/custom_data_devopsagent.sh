21e805c35366d0c396f59491c9a5b6fddb077ea8
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2024-11-18 18:12:32 +0530

    Update custom_data_devopsagent.sh

--- terraform-yelpcamp-azure/main/custom_data_devopsagent.sh
+++ terraform-yelpcamp-azure/main/custom_data_devopsagent.sh
@@ -7,12 +7,9 @@ chmod 600 /home/ritesh/.ssh/authorized_keys;
 chown ritesh:ritesh /home/ritesh/.ssh -R;
 echo "ritesh  ALL=(ALL)  NOPASSWD:ALL" > /etc/sudoers.d/ritesh;
 chmod 440 /etc/sudoers.d/ritesh;
-####################################################################
-mkdir /mederma;
-mkfs.xfs /dev/sdc;
-echo "/dev/sdc  /mederma  xfs  defaults 0 0" >> /etc/fstab;
-mount -a;
+
 #################################################################### DevOps Agent ###########################################################################
+
 useradd -s /bin/bash -m demo;
 echo "Password@#795" | passwd demo --stdin;
 yum install -y vim wget zip unzip
