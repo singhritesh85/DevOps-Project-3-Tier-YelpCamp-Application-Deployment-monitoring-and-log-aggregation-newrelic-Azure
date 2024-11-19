9999171f9bd6fbc0397587257dc24f36d85f4414
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2024-11-19 13:41:07 +0530

    Update custom_data_sonarqube.sh

--- terraform-yelpcamp-azure/main/custom_data_sonarqube.sh
+++ terraform-yelpcamp-azure/main/custom_data_sonarqube.sh
@@ -14,14 +14,41 @@ yum install -y zip unzip vim wget
 useradd -s /bin/bash -m sonar;
 echo "Password@#795" | passwd sonar --stdin;
 echo "sonar  ALL=(ALL)  NOPASSWD:ALL" >> /etc/sudoers
-ulimit -n 65536
-echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
+
+cat >> /etc/sysctl.conf <<EOT
+vm.max_map_count = 262144
+fs.file-max = 65536
+EOT
+
+cat >> /etc/security/limits.conf <<EOF
+sonar   -   nofile   65536
+
+#Nofile is the maximum number of open files
+EOF
+
 sysctl -p
 yum install -y java-17* git
 cd /opt/ && wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.5.90363.zip
 unzip sonarqube-9.9.5.90363.zip
 mv /opt/sonarqube-9.9.5.90363 /opt/sonarqube
 chown -R sonar:sonar /opt/sonarqube
+
+cat > /etc/systemd/system/sonarqube.service <<END_OF_SCRIPT
+[Unit]
+Description=SonarQube service
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=sonar
+Group=sonar
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END_OF_SCRIPT
 
 ####################################################################
 
